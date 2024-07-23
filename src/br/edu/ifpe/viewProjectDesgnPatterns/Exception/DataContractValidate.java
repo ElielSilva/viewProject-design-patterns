d3e1bccab8e58ad2228106afd0d4d000fdc11cd5
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-07-23 02:31:59 -0300

    [FEAT] - Add new exceptions

--- src/br/edu/ifpe/viewProjectDesgnPatterns/Exception/DataContractValidate.java
+++ src/br/edu/ifpe/viewProjectDesgnPatterns/Exception/DataContractValidate.java
@@ -1,2 +1,22 @@
-package br.edu.ifpe.viewProjectDesgnPatterns.Exception;public class DataContractValidate {
+package br.edu.ifpe.viewProjectDesgnPatterns.Exception;
+
+public class DataContractValidate extends Exception {
+    public DataContractValidate() {
+    }
+
+    public DataContractValidate(String message) {
+        super(message);
+    }
+
+    public DataContractValidate(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataContractValidate(Throwable cause) {
+        super(cause);
+    }
+
+    public DataContractValidate(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/br/edu/ifpe/viewProjectDesgnPatterns/Exception/NotFoundEntity.java
+++ src/br/edu/ifpe/viewProjectDesgnPatterns/Exception/NotFoundEntity.java
@@ -1,2 +1,22 @@
-package br.edu.ifpe.viewProjectDesgnPatterns.Exception;public class NotFoundEntity {
+package br.edu.ifpe.viewProjectDesgnPatterns.Exception;
+
+public class NotFoundEntity extends Exception {
+    public NotFoundEntity() {
+    }
+
+    public NotFoundEntity(String message) {
+        super(message);
+    }
+
+    public NotFoundEntity(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundEntity(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundEntity(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
